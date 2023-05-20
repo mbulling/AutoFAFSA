b676761b31083af271f833be47f2727728ff0a80
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2023-05-20 13:43:18 -0400

    student information

---
+++ student_information.py
@@ -0,0 +1,34 @@
+# Information for first section of the FAFSA (Student Information)
+class StudentInformation:
+    lastName = ""
+    firstName = ""
+    middleInitial = ""
+    address = ""
+    city = ""
+    state = ""
+    zipCode = ""
+    social_security_number = ""
+    date_of_birth = ""
+    telephone_number = ""
+    driver_license_number = ""
+    driver_license_state = ""
+    email_address = ""
+    citizenship_status = ""
+    alien_registration_number = ""
+    marital_status = ""
+    marital_date = ""
+    legal_resident_state = ""
+    legal_resident_before_2018 = True
+    legal_resident_date = ""
+    parent_1_education_level = ""
+    parent_2_education_level = ""
+    did_complete_high_school = True
+    high_school_name = ""
+    high_school_city = ""
+    high_school_state = ""
+    bachelor_degree_before_2023 = False
+    grade_in_college = ""
+    degree_type = ""
+
+
+    
